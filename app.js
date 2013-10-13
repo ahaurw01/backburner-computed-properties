9e23c14ff241bafc7dece4dc04a386b8bbc68b9d
ahaurw01 <aaron.haurwitz@gmail.com>
2013-10-13 12:30:32 -0400

    documentation and renaming

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Backburner = window.backburner.Backburner;
 
 var ComputeModel = function (hash) {
-  this.backburner = new Backburner(['sync', 'recompute']);
+  this.backburner = new Backburner(['beforeRecompute', 'recompute', 'afterRecompute']);
   this.backburner.run(function () {
     // Do this all inside of run() so we know baseline values are set before computation
     var key, value;
@@ -10,7 +10,7 @@ var ComputeModel = function (hash) {
         value = hash[key];
         if (value instanceof ComputeModel.ComputedProperty) {
           // Create this computed property
-          this.createComputedProperty(key, value.compute, value.dependentProperties);
+          this.createComputedProperty(key, value);
         } else {
           // Plain old-fashioned key/value pair
           this._values[key] = hash[key];
@@ -31,37 +31,59 @@ ComputeModel.prototype = {
 
   computedProperties: [],
 
+  /**
+   * Retrieve the stored value of the given property
+   * @param {string} key - name of the property
+   */
   get: function (key) {
     return this._values[key];
   },
 
+  /**
+   * Set the value of a property and defer computation of related computed properties
+   * @param {string} key - name of the property
+   * @param {string} value - new value
+   */
   set: function (key, value) {
     // Set the value immediately, defer the computation of related computed properties
     this._values[key] = value;
     this.scheduleSync(key);
   },
 
-  createComputedProperty: function (key, fn, dependentProperties) {
-    this.computedProperties.push({
-      key: key,
-      compute: fn,
-      dependentProperties: dependentProperties
-    });
+  /**
+   * Initialize a computed property. Meant to be called from the constructor.
+   * @private
+   * @param {string} key - name of the property to create
+   * @param {ComputeModel.ComputedProperty} - computed property instance
+   */
+  createComputedProperty: function (key, computedProperty) {
+    computedProperty.key = key;
+    this.computedProperties.push(computedProperty);
     // Kick off eventual computation
     var backburner = this.backburner,
         self = this;
     backburner.run(function () {
-      dependentProperties.forEach(function (dp) {
+      computedProperty.dependentProperties.forEach(function (dp) {
         self.scheduleSync(dp);
       });
     });
   },
 
+  /**
+   * Schedule the eventual change notification of this property
+   * @private
+   * @param {string} key - name of the property that is changing
+   */
   scheduleSync: function (key) {
-    log('Scheduling sync: ' + key);
-    this.backburner.deferOnce('sync', this, 'scheduleRecompute', key); // Don't need to be notified multiple times for this one key
+    log('Scheduling beforeRecompute: ' + key);
+    this.backburner.deferOnce('beforeRecompute', this, 'scheduleRecompute', key); // Don't need to be notified multiple times for this one key
   },
 
+  /**
+   * Schedule the eventual computation of a property
+   * @private
+   * @param {string} changedKey - name of the property that will be recomputed
+   */
   scheduleRecompute: function (changedKey) {
     this.computedProperties.forEach(function (cp) {
       // Does this guy depend on `changedKey`? If so, recompute him.
@@ -72,6 +94,11 @@ ComputeModel.prototype = {
     }.bind(this));
   },
 
+  /**
+   * Recompute the value for the given property
+   * @private
+   * @param {ComputeModel.ComputedProperty} computedProperty
+   */
   recompute: function (computedProperty) {
     // Retrieve the current values for the dependent keys
     var injectedArgs = computedProperty.dependentProperties.map(function (dp) {
@@ -84,10 +111,18 @@ ComputeModel.prototype = {
   }
 };
 
+/**
+ * Computed property constructor
+ * @constructor
+ */
 ComputeModel.ComputedProperty = function (compute, dependentProperties) {
   this.compute = compute;
   this.dependentProperties = dependentProperties;
 };
+
+/**
+ * Create a ComputedProperty instance based off of the function
+ */
 Function.prototype.computed = function () {
   var dependentProperties = Array.prototype.slice.call(arguments);
   return new ComputeModel.ComputedProperty(this, dependentProperties);      
